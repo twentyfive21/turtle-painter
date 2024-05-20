17e9ad5a7c7cf890db0a1609f65667ae0a5dd1f0
twentyfive21 <tina.winnn@gmail.com>
2024-05-20 10:18:21 -0400

    added wait message for circle program does not run until after circle finishes drawing

--- turtle-painter/src/main/java/MainApp.java
+++ turtle-painter/src/main/java/MainApp.java
@@ -12,8 +12,8 @@ public class MainApp
     // ************************* Start of program **************************
     public static void main(String[] args)
     {
-        System.out.println("\n  ** DISCLAIMER** recommended to say within (0,0) if width & height " +
-                "\n are both around 300 or else shape will draw out of bounds\n");
+        System.out.println("\n**** ʕ•́ᴥ•̀ʔ DISCLAIMER ʕ•́ᴥ•̀ʔ ****\nRecommended to stay within (0,30) - (0,0) if width & height " +
+                "\nare both around 300 or else shape will draw out of bounds ****\n");
         getUserShape();
 
     }
@@ -40,7 +40,9 @@ public class MainApp
     }
 
     public static void getInput(String type){
-
+            int radius = 0;
+            System.out.print("Color: ");
+            String color = scanner.nextLine().toLowerCase();
             System.out.print("x axis: ");
             int x = scanner.nextInt();
             System.out.print("y axis: ");
@@ -48,30 +50,30 @@ public class MainApp
             int[] cord = {x,y};
             System.out.print("Border width(pen width): ");
             int border = scanner.nextInt();
-            System.out.println("\n** HINT Try to do a width/height around 350+ To not get a small canvas **");
+            System.out.println("\n** HINT Try to do a width/height around 350+ To not get a small canvas **\n");
             System.out.print("Width: ");
             int width = scanner.nextInt();
             System.out.print("Height: ");
             int height = scanner.nextInt();
+            if(type.equals("circle")) {
+                System.out.print("Radius: ");
+                radius = scanner.nextInt();
+                scanner.nextLine();
+                getShapeInfo(cord,border,width,height,color,radius);
+            }
             scanner.nextLine();
-            // 400 at least
-            System.out.print("Color: ");
-            String color = scanner.nextLine().toLowerCase();
+
 
         if(type.equals("square") || type.equals("triangle")){
             getShapeInfo(type,cord,border,width,height,color);
         }
 
-        if(type.equals("circle")){
-            System.out.print("Radius: ");
-            int radius = scanner.nextInt();
-            scanner.nextLine();
-            getShapeInfo(cord,border,width,height,color,radius);
-        }
+
 
     }
 
     public static void getShapeInfo(String type,int[]cord, int border, int width, int height, String color){
+        System.out.println("\n~~~~ Drawing shape...Creating magic (ε(*´･ω･)っ†*ﾟ¨ﾟﾟ･*:..☆〜♡॰ॱ ~~~~\n");
         if(type.equals("square")){
             // int[] coordinate, String color, int border, int width, int height
             Square square = new Square(cord,color,border,width,height);
@@ -86,8 +88,10 @@ public class MainApp
     }
 
     public static void getShapeInfo(int[]cord, int border, int width, int height, String color, int radius){
+        System.out.println("\n~~~~ Drawing shape...Creating magic (ε(*´･ω･)っ†*ﾟ¨ﾟﾟ･*:..☆〜♡॰ॱ ~~~~\n");
+        System.out.println("\n !!! Please wait while circle draws for program to continue !!!\n");
         // int[] coordinate, String color, int border, int width, int height, int radius
-        Circle circle = new Circle(cord, color, border,width,height,radius);
+        Circle circle = new Circle(cord,color,border,width,height,radius);
         circle.setColor();
         circle.paint();
     }
